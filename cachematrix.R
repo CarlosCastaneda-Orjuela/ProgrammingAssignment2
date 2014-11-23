3ebebe60de5b65d4a04641bd6be4f916c7a3d2c4
CarlosCastaneda-Orjuela <cacastanedao@unal.edu.co>
2014-11-23 14:00:01 -0500

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -16,7 +16,6 @@ makeCacheMatrix <- function(x = matrix()) {
              setinverse = setinverse,
              getinverse = getinverse)
 }
-}
 
 
 ## Write a short comment describing this function
